68673b2a5d0644d232901cca91bc73e1df9641d4
eszter-b <buriseszter@gmail.com>
2025-12-18 15:18:21 +0100

    update dependencies in build files

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         "TM1py>=2.1,<3.0",
         "requests>=2.25",
-        "tm1_bedrock_py>=1.0.2,<=1.1.1",
+        "tm1_bedrock_py==1.1.3",
     ],
     python_requires=">=3.8",
 )
