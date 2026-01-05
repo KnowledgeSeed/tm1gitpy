f86304d9508e464e7400f5e9fd83723ac8e631fb
Nagy Ákos András <nagy.akos.andras@knowledgeseed.local>
2026-01-05 09:43:37 +0100

    Adding nuitka win exe

---
+++ bin/win/tm1gitpy.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'tm1gitpy.exe' with Python DLL being found.
+set PATH=c:\users\nagy.akos.andras\appdata\local\programs\python\python313;%PATH%
+set PYTHONHOME=c:\Users\nagy.akos.andras\AppData\Local\Programs\Python\Python313
+set NUITKA_PYTHONPATH=C:\Users\nagy.akos.andras\tm1_git_py;c:\Users\nagy.akos.andras\AppData\Local\Programs\Python\Python313\DLLs;c:\Users\nagy.akos.andras\AppData\Local\Programs\Python\Python313\Lib;c:\Users\nagy.akos.andras\AppData\Local\Programs\Python\Python313;C:\Users\nagy.akos.andras\tm1_git_py\.venv;C:\Users\nagy.akos.andras\tm1_git_py\.venv\Lib\site-packages;C:\Users\nagy.akos.andras\tm1_git_py\.venv\Lib\site-packages\win32;C:\Users\nagy.akos.andras\tm1_git_py\.venv\Lib\site-packages\win32\lib;C:\Users\nagy.akos.andras\tm1_git_py\.venv\Lib\site-packages\Pythonwin
+"%~dp0tm1gitpy.exe" %*
